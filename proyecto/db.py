c99e6a7772cb1da055e5c16198d6134b5366d4ad
Alejandro Herrera <brayan.herrera@utp.edu.co>
2021-11-18 23:30:32 -0500

    Add Any changes

--- proyecto/db.py
+++ proyecto/db.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from .config import config
+from config import config
+
 
 print(config.get("database"))
 conn = create_engine("postgresql://beta_sd:lag404@postgres/clima")
